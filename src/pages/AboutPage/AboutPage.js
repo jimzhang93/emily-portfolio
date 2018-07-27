839cd6200ed67951304a3ae2f004af08ff78a45f
Jimmy Zhang <jimzhang93@gmail.com>
2018-07-27 18:55:04 -0400

    Adds all assets

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -25,13 +25,16 @@ const AboutPage = (props, context) => {
         <div className="about-wrapper">
           <div className="about-content" style={{ color: textPrimary }}>
             <p>
-              <span className="highlight"><b>Emily Zhang</b></span> is a Chinese-American visual artist, photographer, and student from Atlanta, Georgia. A <b>2017 National YoungArts foundation finalist</b> and <b>2018 Scholastic Art and Writing Awards national gold medalist</b>, she has been recognized by the Alliance for Young Artists and Writers, and has been published in The Adroit Journal and TRACK//FOUR journal. Her work has appeared in exhibitions at Savannah College of Art and Design, Miami Art District’s YoungArts Foundation, Pratt Institute, and New York University’s Steinhardt Rosenberg Gallery, among others.
+              <span className="highlight"><b>Emily Zhang</b></span> is a Chinese-American visual artist, photographer, and student from the Atlanta, Georgia area. A <b>2017 National YoungArts foundation finalist and 2018 Scholastic Art and Writing Awards national gold medalist</b>, she has been recognized by the Alliance for Young Artists and Writers, and has been published in The Adroit Journal and TRACK//FOUR journal. Her work has appeared in exhibitions at Savannah College of Art and Design, Miami Art District’s YoungArts Foundation, Pratt Institute, and New York University’s Steinhardt Rosenberg Gallery, among others.
             </p>
             <p>
               Because her work primarily focuses on her own <b>family roots, Chinese history, and the “model minority” experience</b>, much of her inspiration comes from her experiences exploring areas of old Shanghai, where all of her extended family resides and where both of her parents were born and raised.
             </p>
             <p>
-              She has received mentorship from master multidisciplinary artists such as Derrick Adams, Naomi Fisher, and Watermark Contemporary Gallery artist and RISD Alum Kristen Glosser. In addition, her work has been featured in galleries curated by Director of Perez Art Museum Miami, Franklin Sirmans.
+              She has received <b>mentorship from master multidisciplinary artists</b> such as Derrick Adams, Naomi Fisher, and Watermark Contemporary Gallery artist and RISD Alum Kristen Glosser. In addition, her work has been featured in galleries curated by Director of Perez Art Museum Miami, Franklin Sirmans.
+            </p>
+            <p>
+              If you wish to connect, I'd love to chat! I can be reached at <b>emzhang00@gmail.com</b>. Thank you for visiting!
             </p>
           </div>
         </div>
